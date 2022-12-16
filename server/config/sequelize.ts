96198b8388dbba7fd74804489e53e7be1a1dadc7
Mateo-Le-Fur <101249799+Mateo-Le-Fur@users.noreply.github.com>
2022-12-16 16:52:47 +0100

    private messages done

--- server/config/sequelize.ts
+++ server/config/sequelize.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  "postgres://chat:MuvzH6712Hg@@localhost/chat" as string,
+  process.env.PG_URL as string,
   {
     define: {
       createdAt: "created_at",
-      updatedAt: "updated_at",
+      updatedAt: "updated_at"
     },
 
     // benchmark: true,
-    logging: false,
+    logging: false
   }
 );
 
